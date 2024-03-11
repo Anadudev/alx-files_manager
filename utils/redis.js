4ee76f5fdba3e29dc8a1ee9398036383f8049354
Mbang Ernest N <mbangernest@yahoo.com>
2024-03-11 16:05:23 +0100

    restructure methods

--- utils/redis.js
+++ utils/redis.js
@@ -1,31 +1,52 @@
-import { promisify } from 'util';
-import { createClient } from 'redis';
+import redis from 'redis';
+import util from 'util';
 
 class RedisClient {
-  constructor () {
-    this.client = createClient()
-      .on('error', (error) => {
-        console.log(`${error.message}`);
-      });
-  }
+    constructor() {
+        this.client = redis.createClient();
 
-  isAlive () {
-    return this.client.connected;
-  }
+        // Promisify Redis client methods
+        this.getAsync = util.promisify(this.client.get).bind(this.client);
+        this.setAsync = util.promisify(this.client.set).bind(this.client);
+        this.delAsync = util.promisify(this.client.del).bind(this.client);
 
-  async get (key) {
-    const value = await promisify(this.client.GET).bind(this.client)(key);
-    return (value);
-  }
+        // Listen for errors
+        this.client.on('error', (err) => {
+            console.error(err);
+        });
+    }
 
-  async set (key, value, duration) {
-    await promisify(this.client.SET).bind(this.client)(key, value, 'EX', duration);
-  }
+    isAlive() {
+        return this.client.connected;
+    }
 
-  async del (key) {
-    await promisify(this.client.DEL).bind(this.client)(key);
-  }
+    async get(key) {
+        try {
+            const value = await this.getAsync(key);
+            return value;
+        } catch (error) {
+            console.error('Error getting value from Redis:', error);
+            return null;
+        }
+    }
+
+    async set(key, value, durationInSeconds) {
+        try {
+            await this.setAsync(key, value, 'EX', durationInSeconds);
+        } catch (error) {
+            console.error('Error setting value in Redis:', error);
+        }
+    }
+
+    async del(key) {
+        try {
+            await this.delAsync(key);
+        } catch (error) {
+            console.error('Error deleting value from Redis:', error);
+        }
+    }
 }
 
-export const redisClient = new RedisClient();
+// Create and export an instance of RedisClient
+const redisClient = new RedisClient();
 export default redisClient;
