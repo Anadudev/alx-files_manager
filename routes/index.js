6b1648b793faf1ae2eed39404eaef2e571123dff
Anadudev <anadudev@gmail.com>
2024-03-12 07:09:01 -0700

    handlig file erquired datas

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
 
@@ -17,4 +18,7 @@ router.post('/users', UsersController.postNew);
 
 // // UserController endpoints
 // router.get('users/me', UserController.getMe);
+
+// just for testig /files route
+router.post('/files', FilesController.postUpload);
 export default router;
