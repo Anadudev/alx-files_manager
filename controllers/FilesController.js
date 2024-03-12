da6c821ba4b65709857e5ba3c7ccdaea37399d42
AnaduCG <godzionwin@gmail.com>
2024-03-12 15:26:36 +0100

    eslinitig

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,32 +1,31 @@
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
-
 const FilesController = {
-	// /files should create a new file in DB and in disk:
-	postUpload: async (req, res) => {
-		try{
-			const { headers } = req;
-			const { body } = req;
-			const token = headers['x-token'];
-			const user = await redisClient.get(redisClient);
-			console.log(body);
-			if (!body.name){
-				return res.status(400).json({Error: "Missing name"});
-			}
-			if (!body.type){
-				return res.status(400).json({Error: "Missing type"});
-			}
-			if (!body.data && body.type != 'folder'){
-				return res.status(400).json({Error: "Missing data"});
-			}
-			if (!user){
-				return res.status(401).json({Error: "Unauthorized"});
-			}
-		} catch (error){
-			console.log(`Error ${error}`);
-		}
-		return res;
-	}
-}
-export default FilesController;	
+  // /files should create a new file in DB and in disk:
+  postUpload: async (req, res) => {
+    try {
+      const { headers } = req;
+      const { body } = req;
+      const token = headers['x-token'];
+      const user = await redisClient.get(redisClient);
+      console.log(body);
+      if (!body.name) {
+        return res.status(400).json({ Error: 'Missing name' });
+      }
+      if (!body.type) {
+        return res.status(400).json({ Error: 'Missing type' });
+      }
+      if (!body.data && body.type !== 'folder') {
+        return res.status(400).json({ Error: 'Missing data' });
+      }
+      if (!user) {
+        return res.status(401).json({ Error: 'Unauthorized' });
+      }
+    } catch (error) {
+      console.log(`Error ${error}`);
+    }
+    return res;
+  },
+};
+export default FilesController;
