996a603b50d761e4eba2e795a582ddfbd8f601a5
Mbang Ernest N <mbangernest@yahoo.com>
2024-03-12 17:01:57 +0100

    fix: modify creation and deletion functions

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -6,43 +6,43 @@ import { redisClient } from '../utils/redis';
 const AuthController = {
   getConnect: async (req, res) => {
     const authHeader = req.headers.authorization;
-    if (authHeader) {
-      // Parse the header to extract credentials if needed
-      const encodedCredentials = authHeader.split(' ')[1];
-      const decodedCredentials = Buffer.from(encodedCredentials, 'base64').toString('utf-8');
-      const [email, password] = decodedCredentials.split(':');
 
-      try {
-        const checkUser = await dbClient.client.db().collection('users').findOne({ email, password: sha1(password) });
-        if (!checkUser) {
-          res.status(401).json({ error: 'Unauthorized' });
-          return;
-        }
-        const token = `auth_${uuidv4()}`;
-        await redisClient.set(token, checkUser._id, 86400);
-        res.status(200).json({ token: token.split('_')[1] });
-      } catch (error) {
-        console.log(`Some error occurred: ${error}`);
-        res.status(500).json({ error: 'Internal server error' });
+    if (!authHeader || !authHeader.startsWith('Basic ')) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    // Parse the header to extract credentials if needed
+    const encodedCredentials = authHeader.split(' ')[1];
+    const decodedCredentials = Buffer.from(encodedCredentials, 'base64').toString('utf-8');
+    const [email, password] = decodedCredentials.split(':');
+
+    try {
+      const checkUser = await dbClient.client.db().collection('users').findOne({ email, password: sha1(password) });
+      if (!checkUser) {
+        return res.status(401).json({ error: 'Unauthorized' });
       }
+      const token = `auth_${uuidv4()}`;
+      await redisClient.set(token, checkUser._id, 86400);
+      return res.status(200).json({ token: token.split('_')[1] });
+    } catch (error) {
+      console.log(`Some error occurred: ${error}`);
+      return res.status(500).json({ error: 'Internal server error' });
     }
   },
   getDisconnect: async (req, res) => {
     const authHeaderToken = req.headers['x-token'];
-    if (authHeaderToken) {
-      // Authentication token is present
-      try {
-        const checkToken = redisClient.get(`auth_${authHeaderToken}`);
-        if (!checkToken) {
-          res.status(401).json({ error: 'Unauthorized' });
-          return;
-        }
-        await redisClient.del(`auth_${authHeaderToken}`);
-        res.status(204).send();
-      } catch (error) {
-        console.log(`Some error occurred: ${error}`);
-        res.status(500).json({ error: 'Internal server error' });
+    if (!authHeaderToken) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    try {
+      const checkToken = redisClient.get(`auth_${authHeaderToken}`);
+      if (!checkToken) {
+        return res.status(401).json({ error: 'Unauthorized' });
       }
+      await redisClient.del(`auth_${authHeaderToken}`);
+      return res.status(204).send();
+    } catch (error) {
+      console.log(`Some error occurred: ${error}`);
+      return res.status(500).json({ error: 'Internal server error' });
     }
   },
 };
